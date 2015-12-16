e4207406e63cca2081dbd90f3a768ba48fc90836
Martin <mk522@njit.edu>
2015-12-16 15:46:04 -0500

    Done, need to work on Session

---
+++ Deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tar -xvzf /home/martin/Desktop/Packages/$1.tar.gz -C /
--- MovePackage.sh
+++ MovePackage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-
+scp /home/martin/Desktop/Packages/$1.tar.gz martin@192.168.1.19:/home/martin/Desktop/Packages/$1.tar.gz
