d591add62f3b425d5359feec1e3c0d9fd6f6af13
Martin <mk522@njit.edu>
2015-12-18 13:35:06 -0500

    LiveV3

--- Deploy.sh
+++ Deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 tar -xvzf /home/martin/Desktop/Packages/$1.tar.gz -C /
+cp /home/martin/Desktop/Packages/$1.tar.gz /home/martin/Desktop/Backup/$1.tar.gz
--- MakePackage.sh
+++ MakePackage.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 tar -zcvf /home/martin/Desktop/Packages/$1.tar.gz --exclude='/home/martin/Documents/RabbitMQ.ini' --exclude='/home/martin/Documents/RabbitMQ.ini~' --exclude='/home/martin/Documents/loginBackup.sql' --exclude='/home/martin/Documents/log.txt' --exclude='/home/martin/Documents/log.txt~' /home/martin/Documents/*
+
+cp /home/martin/Desktop/Packages/$1.tar.gz /home/martin/Desktop/Backup/$1.tar.gz
